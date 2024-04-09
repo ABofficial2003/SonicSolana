b660cb9e59223fef38d3b5e5c04fc6749f88b7cb
Adarsh Biswas <112661969+ABofficial2003@users.noreply.github.com>
2024-04-09 18:46:33 +0530

    semi final

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
-let page1 = document.getElementById("landing");
-let page2 = document.getElementById("coming-soon");
+// let page1 = document.getElementById("landing");
+// let page2 = document.getElementById("coming-soon");
 
-const changeBg = () => {
-  let tl = gsap.timeline();
+// const changeBg = () => {
+//   let tl = gsap.timeline();
 
-  gsap.to(page1,{
-    opacity: 0,
-    // scale:0,
-    ease:Power2,
-    duration:1
-  })
+//   gsap.to(page1,{
+//     opacity: 0,
+//     // scale:0,
+//     ease:Power2,
+//     duration:1
+//   })
 
-  gsap.to(page2, {
-    zIndex: 12,
-    duration: 0.5,
-    ease:Power1,
-    // scale: 1,
-    opacity: 1,
-  });
-};
+//   gsap.to(page2, {
+//     zIndex: 12,
+//     duration: 0.5,
+//     ease:Power1,
+//     // scale: 1,
+//     opacity: 1,
+//   });
+// };
